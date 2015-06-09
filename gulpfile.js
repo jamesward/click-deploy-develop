3c23123f53a32d4aba75b9cc13a561f8af0580f7
James Ward <james@jamesward.org>
2015-06-09 12:00:22 -0600

    install gulp-control Atom package

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
   gutil = require('gulp-util'),
   pkg = require('./package.json');
 
+// load .env into process.env
+require('dotenv').load();
 
 // Start a server with livereload support
 gulp.task('server', function() {
@@ -20,13 +22,25 @@ gulp.task('server', function() {
 });
 
 
-// Run Atom
-gulp.task('atom', function() {
+// Install the gulp-control Atom package and then run Atom
+gulp.task('atom', function(cb) {
   var atomExePath = require('gulp-atom-downloader');
   var proc = require('child_process');
 
-  return atomExePath().then(function(atomExePath) {
-    return proc.spawn(atomExePath, ['./', 'README.md']);
+  atomExePath().then(function(atomExePath) {
+    var apm = require('atom-package-manager/lib/apm-cli');
+    // quietly install gulp-control
+    apm.run(['install', '-q', 'gulp-control'], function(error) {
+      if (error) {
+        cb(error);
+      }
+      else {
+        proc.spawn(atomExePath, ['./', 'README.md']).on('close', function(code) {
+          if (code == 0) cb();
+          else cb('Atom shutdown with an error.');
+        });
+      }
+    });
   });
 });
 
@@ -34,16 +48,21 @@ gulp.task('atom', function() {
 // Deploy the app on Heroku
 gulp.task('heroku-deploy', function() {
   var apiToken = process.env.HEROKU_API_TOKEN;
-  var appName = process.env.HEROKU_APP_NAME || require("path").basename(__dirname);
+  var appName = process.env.HEROKU_APP_NAME || require('path').basename(__dirname);
+
+  if (apiToken == null) {
+    throw new gutil.PluginError(pkg.name, 'You must set a HEROKU_API_TOKEN in a .env file.');
+  }
 
   return gulp.src(['app.js', 'gulpfile.js', 'package.json', 'gulp', 'gulp.exe'])
     .pipe(require('gulp-tar')('app.tar'))
     .pipe(require('gulp-gzip')())
     .pipe(require('gulp-heroku-source-deployer').deploy(apiToken, appName))
-    .on("data", function(buildInfo) {
-      gutil.log("Deploy has begun!  Watch the progress:\n" +
-        "https://dashboard.heroku.com/apps/" + appName + "/activity/builds/" + buildInfo.id);
+    .on('data', function(buildInfo) {
+      gutil.log('Deploy has begun!  Watch the progress:\n' +
+        'https://dashboard.heroku.com/apps/' + appName + '/activity/builds/' + buildInfo.id);
     });
+    // todo: better error handling when the app isn't found
 });
 
 
